67405d78ac17770c8d5eaf5840cf94df9be9966d
segfault-bilibili <segfault_bilibili@qq.com>
2021-08-31 10:26:41 +0800

    指定dev server监听127.0.0.1，避免出现真机不能热加载的问题

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    //Hot reloading may not work on other device,
+    //because it may send requests to LAN IP address,
+    //which is not accessible (either simply unreachable or blocked by firewall)
+    host: '127.0.0.1',
+  }
+}
