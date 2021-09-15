a9f8d217cd9d16d3ceaca0a36d608eadbdf4ff38
segfault-bilibili <segfault_bilibili@qq.com>
2021-09-16 01:55:49 +0800

    打包字体而不是从googleapis或bootcss的CDN下载(否则没网时webview会白屏)

--- src/main.js
+++ src/main.js
@@ -9,6 +9,12 @@ import FastClick from 'fastclick'
 FastClick.attach(document.body)
 // flexible
 import '../public/js/flexible.js'
+// Roboto fonts
+// <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic">
+import './Roboto/Roboto_300,400,500,700,400italic.css'
+// Material Design icons
+// <link rel="stylesheet" href="https://cdn.bootcss.com/material-design-icons/3.0.1/iconfont/material-icons.css">
+import './material-design-icons/3.0.1/iconfont/material-icons.css'
 
 Vue.config.productionTip = false
 
